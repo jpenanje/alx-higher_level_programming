24ede68c4096521903ae62193cb9a00cd6a87179
jpenanje <jpenanje@gmail.com>
2022-06-01 17:29:08 +0100

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str1 = ""
+    for i in range(len(str)):
+        if ord(str[i]) > 96 and ord(str[i]) < 123:
+            str1 += chr(ord(str[i]) - 32)
+        else:
+            str1 += str[i]
+    print("{}".format(str1))
