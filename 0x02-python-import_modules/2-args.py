34f707d3bff4a35f2889e96e961c05e5b5c72504
jpenanje <jpenanje@gmail.com>
2022-06-02 23:44:11 +0100

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argcount = len(sys.argv) - 1
+    if argcount == 0:
+        print("0 arguments.")
+    else:
+        if argcount == 1:
+            print("1 argument:")
+        else:
+            print("{} arguments:")
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
