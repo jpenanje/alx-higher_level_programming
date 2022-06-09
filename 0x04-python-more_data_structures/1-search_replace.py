e602b0f00e4a49e56a8fbd04a032444819553ea6
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-06-09 08:08:52 +0100

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    mylist = my_list.copy()      
+    mylist = my_list.copy()   
     for i in range(0, len(mylist)):
         if mylist[i] == search:
             mylist[i] = replace
