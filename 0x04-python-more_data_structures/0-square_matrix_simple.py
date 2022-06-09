a420c8dbdfcb2f5a450e282cdfd2cd881952b42b
jpenanje <jpenanje@gmail.com>
2022-06-09 07:35:47 +0100

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     square_matrix = []
     for row in matrix:
-        square_matrix.append([col ** 2 for con in row])
+        square_matrix.append([col ** 2 for col in row])
     return square_matrix
