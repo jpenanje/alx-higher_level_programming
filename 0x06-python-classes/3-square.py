6759051fa9b57b58935dc6ed56a030d6e9a28a55
jpenanje <jpenanje@gmail.com>
2022-06-22 00:51:07 +0100

    class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Create a class called Square"""
+
+
+class Square:
+    """A Square class that defines a square by it's size
+    """
+    def __init__(self, size=0):
+        """Instantiate a square
+        Args:
+            size: size of square
+        """
+        if type(size) == int:
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Calculate area of square
+        Returns:
+            area of square
+        """
+        return (size * size)
