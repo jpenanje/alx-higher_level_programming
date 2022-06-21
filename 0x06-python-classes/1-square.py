40ea5b67a8efd3ceee93ead0ef2aad08bc09a8c7
jpenanje <jpenanje@gmail.com>
2022-06-21 23:20:16 +0100

    Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Create a class called Square"""
+
+
+class Square:
+    """A Square class that defines a square by it's size
+    """
+    def __init__(self, size):
+        """Instantiate a square
+        Args:
+            size: size of square
+        """
+        self.size = size
