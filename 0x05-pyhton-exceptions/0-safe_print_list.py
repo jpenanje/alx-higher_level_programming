eb6ba576d06641aacd79cd49ff708898439077dc
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-06-21 01:42:11 +0100

    Update 0-safe_print_list.py

--- 0x05-pyhton-exceptions/0-safe_print_list.py
+++ 0x05-pyhton-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[elm], end="")
             i += 1
-        except IndexError:
+        except Exception as err:
             break
     print('')
     return i
