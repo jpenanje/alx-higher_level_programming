32cbe40d3e39bccda554c6f33ee05040550dba23
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-07-13 19:13:51 +0100

    Update 7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -52,7 +52,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return rect
         for i in range(self.__height):
-            rect += (str(type(self).print_symbol) * self.__width) + "\n"
+            rect += (str(self.print_symbol) * self.__width) + "\n"
         return rect[:-1]
 
     def __repr__(self):
