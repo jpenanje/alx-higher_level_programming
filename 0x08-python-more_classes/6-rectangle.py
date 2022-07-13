183eba13a2467ad22e355eb6b5619c7346264d26
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-07-13 19:03:11 +0100

    Update 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
-        number_of_instances += 1
+        self.number_of_instances += 1
         self.width = width
         self.height = height
 
@@ -58,5 +58,5 @@ class Rectangle:
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __del__(self):
-        number_of_instances -= 1
+        self.number_of_instances -= 1
         print("Bye rectangle...")
