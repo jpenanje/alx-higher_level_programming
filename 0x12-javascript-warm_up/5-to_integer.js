46f8d292ccde930e2bbb0a913a9f1afa68c59460
jpenanje <jpenanje@gmail.com>
2022-09-06 00:37:30 +0100

    JavaScript warm up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const argument = parseInt(process.argv[2]);
-if (isNaN(argument)) {
+if (!(isNaN(argument))) {
   console.log('My number: ' + argument);
 } else {
   console.log('Not a number');
