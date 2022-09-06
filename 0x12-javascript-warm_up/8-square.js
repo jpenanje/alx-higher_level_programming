28392aa93b8ad4911169de024d68e732dffc11fe
jpenanje <jpenanje@gmail.com>
2022-09-06 01:03:54 +0100

    JavaScript warm up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,8 +3,8 @@ const size = parseInt(process.argv[2]);
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  let square = '';
-  for (let i = 0; i < size; i++) {
+  for (let i = 0, square; i < size; i++) {
+    square = '';
     for (let j = 0; j < size; j++) {
       square += 'X';
     }
