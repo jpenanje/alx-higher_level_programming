d8e739a66b1d495e74d198a6ca073410eb38347f
jpenanje <jpenanje@gmail.com>
2022-09-08 01:36:31 +0100

    OSC

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -10,5 +10,17 @@ class Rectangle {
   print () {
     console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
   }
+
+  rotate () {
+    const h = this.height;
+    const w = this.width;
+    this.width = h;
+    this.height = w;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
 }
 module.exports = Rectangle;
