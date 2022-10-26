13e04043b751419edfc3f954a0215535a37b8864
jpenanje <jpenanje@gmail.com>
2022-10-26 11:54:52 +0100

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+list of attributes and methods of an object.
+"""
+
+
+def lookup(obj):
+    """
+    Return list of attributes and methods
+
+    Args:
+        - obj: Parent class
+    """
+
+    return dir(obj)
